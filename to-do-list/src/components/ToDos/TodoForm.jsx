e058874df0d3ad406906a9641b2641045c9bea0d
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-09-08 19:04:33 +0300

    form added

---
+++ to-do-list/src/components/ToDos/TodoForm.jsx
@@ -0,0 +1,10 @@
+const TodoForm = () => {
+    return (
+        <form>
+            <input placeholder="Enter New To-Do Task" />
+            <button type='submit'>Submit</button>
+        </form>
+    );
+};
+
+export default TodoForm;
