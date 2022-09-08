64832be3aad647e9e91d9a4d0036037c7af7b04a
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-09-08 19:07:18 +0300

    app.js adding content

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import TodoForm from './components/ToDos/TodoForm';
+import TodoList from './components/ToDos/TodoList';
 
 function App() {
   return (
     <div>
-      a
+      <h1>To-Do App</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- to-do-list/src/components/ToDos/Todo.jsx
+++ to-do-list/src/components/ToDos/Todo.jsx
@@ -1,6 +1,10 @@
 const Todo = () => {
     return (
-        <h1>This Is To-Do List</h1>
+        <>  <h1>This Is To-Do List</h1>
+            <h1>This Is To-Do List</h1>
+            <h1>This Is To-Do List</h1>
+        </>
+
     );
 };
 
