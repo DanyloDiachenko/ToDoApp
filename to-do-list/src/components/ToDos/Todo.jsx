1d76d890aac9a9f057757f966fd4b6589e227639
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-09-15 20:25:52 +0300

    fixes

--- to-do-list/src/components/ToDos/Todo.jsx
+++ to-do-list/src/components/ToDos/Todo.jsx
@@ -3,9 +3,9 @@ import styles from './Todo.module.css';
 const Todo = ({ todo }) => {
     return (
         <section style={{padding: '0 15px'}}>
-            <div className={styles.todo}>
+            <article className={styles.todo}>
                 <div className={styles.todoText}>{todo}</div>
-            </div>
+            </article>
         </section>
     );
 };
--- to-do-list/src/components/ToDos/TodoList.jsx
+++ to-do-list/src/components/ToDos/TodoList.jsx
@@ -3,6 +3,7 @@ import Todo from './Todo';
 const TodoList = ({ todos }) => {
     return (
         <>
+            {!todos.length && <h2>To-Do list is empty...</h2>}
             {todos.map((todo, index) => <Todo key={index} todo={todo} />)}
         </>
     );
